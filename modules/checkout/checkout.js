72a85b3d3fc2467d1ab68e71ee2105f21abc9477
Marco Au <marco.au.321@gmail.com>
2014-07-29 17:58:53 -0700

    Completion of checkout progress bar

--- modules/checkout/checkout.js
+++ modules/checkout/checkout.js
@@ -2,18 +2,30 @@ angular.module('app.checkout', [])
   .controller('CheckoutCtrl', ['$state', '$scope', function($state, $scope){
     $scope.goToShipping = function(){
       $state.go('checkout.shipping');
+      $scope.checkoutState = 'shipping';
+      // $scope.updateCheckoutState();
     }
     $scope.goToBilling = function(){
       $state.go('checkout.billing');
+      $scope.checkoutState = 'billing';
+      $scope.updateCheckoutState();
     };
     $scope.goToConfirm = function(){
       $state.go('checkout.confirm');
+      $scope.checkoutState = 'confirm';
+      $scope.updateCheckoutState();
     };
     $scope.goToThankyou = function(){
       $state.go('checkout.thankyou');
     };
 
-    $state.go('checkout.confirm');
+    $scope.progressBarState = {
+      'shipping': 'bar-shipping',
+      'billing': 'bar-billing',
+      'confirm': 'bar-confirm'
+    };
+
+    $scope.goToShipping();
 
     $scope.shippingInfo = {
       name: 'Irene Chan',
